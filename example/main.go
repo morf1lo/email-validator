7e6dde7e824f6e5521a89ec9caec5a8237dcf8dd
morf1lo <null>
2024-01-15 18:00:17 +0500

    Example

--- example/main.go
+++ example/main.go
@@ -7,11 +7,11 @@ import (
 )
 
 func main() {
-	email := "blablabla@example.lol"
+	email := "blablabla@example.lol" // Valid email format
 	if err := emailvalidator.Valid(email); err != nil {
 		fmt.Println(err)
 		return
 	}
-	
+
 	fmt.Println("Email is OK")
 }
